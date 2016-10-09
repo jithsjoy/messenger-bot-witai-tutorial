7b666c17ff63c7b141cb08d78a188cfe9194e92f
Jiths Joy <Jiths Joy>
2016-10-09 15:18:28 +0530

    'update'

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ if (!WIT_TOKEN) {
 }
 
 
-var FB_PAGE_TOKEN = process.env.FB_PAGE_TOKEN || 'EAAPCnjgJAvkBACZBidHZBZACz35igIGqULElTntgiAgVzIxVxAX7Y38Eb1N2FD2bKGZBT8OlL6a6F0qEJsWhYdWeZCdsbaZCTd13HT8QEwNPRa3owTNuAeqLG5HZAAzkOC1jiGhR90Nlb8mk3cylI0FsuZBf6lWWzMB5ZA3cXXVzgCQZDZD';
+var FB_PAGE_TOKEN = process.env.FB_PAGE_TOKEN || 'EAAJLVzS5xh8BAF8cnwdXdH7BNh9qu33WGgD01z6GLFHeTdmGKidn9t2irq8yieJrY8IpWPXZC4JhXe7opUdJrdy1W6KXZAgxdMuoo8QNhTt4pGgLaHj3w1npbkl34TwFNGy5dJwMUWY0JxxB3nICUfXn3Nmz79CS1ZBMmZAMagZDZD';
 if (!FB_PAGE_TOKEN) {
 	throw new Error('Missing FB_PAGE_TOKEN. Go to https://developers.facebook.com/docs/pages/access-tokens to get one.')
 }
